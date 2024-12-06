9e759473975ee255d923bc5e8dc792929fbfc2b3
Мария Задорина <shmv13m@yandex.ru>
2024-12-06 17:25:48 +0700

    3

--- ya_note/notes/tests/test_logic.py
+++ ya_note/notes/tests/test_logic.py
@@ -1,7 +1,6 @@
 from http import HTTPStatus
 
 from django.contrib.auth import get_user_model
-from django.db import IntegrityError
 from django.test import Client, TestCase
 from django.urls import reverse
 from pytils.translit import slugify
